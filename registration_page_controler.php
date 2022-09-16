0c8bf745887454c9e6aeade67d4579069b4fed1c
Ilija Radovanovic <ilija009@gmail.com>
2022-09-16 21:28:46 +0200

    add registration page

---
+++ registration_page_controler.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . '/views/_layout/v-header.php';
+
+require __DIR__ . '/views/v-registration.php';
+
+require __DIR__ . '/views/_layout/v-footer.php';
---
+++ views/v-registration.php
@@ -0,0 +1,18 @@
+<div class="form-login">
+    <h2>Registration</h2>
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="username">Username</label><br>
+            <input type="text" id="username" name="username">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label><br>
+            <input type="email" id="email" name="email">
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label><br>
+            <input type="password" id="password" name="password">
+        </div>
+        <button class="btn-login" type="submit">Login</button>
+    </form>
+</div>
