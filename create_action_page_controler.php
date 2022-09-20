7e431811c9ceb19e34e7dd389827674edab1f8fe
Ilija Radovanovic <ilija009@gmail.com>
2022-09-20 12:20:38 +0200

    take daughter to school. Change input to textareat at v-create_action.php

--- create_action_page_controler.php
+++ create_action_page_controler.php
@@ -1,7 +1,15 @@
 <?php
 session_start();
 
+// require_once __DIR__ . '/models/DB.php';
+// require_once __DIR__ . '/models/Users.php';
+require_once __DIR__ . '/models/test_input.php';
+
 $page = 'Create action page';
+
+if(isset($_POST['create_action'])) {
+    
+}
 
 require __DIR__ . '/views/_layout/v-header.php';
 
--- views/v-create_action.php
+++ views/v-create_action.php
@@ -1,6 +1,6 @@
 <section class="create-akcija">
     <h2>New Action</h2>
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
         <div class="form-group">
             <label for="image">Image</label><br>
             <input type="text" id="image" name="image">
@@ -9,10 +9,11 @@
             <label for="title">Title</label><br>
             <input type="title" id="title" name="title">
         </div>
+        <!-- NOTE: change input into textarea -->
         <div class="form-group">
             <label for="description">Description</label><br>
             <input type="description" id="description" name="description">
         </div>
-        <button class="btn-login" type="submit">Create</button>
+        <button class="btn-login" type="submit" name="create_action">Create</button>
     </form>
 </section>
