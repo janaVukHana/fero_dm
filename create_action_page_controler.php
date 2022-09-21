dd46d58d7e29258cfe5e514383989844df8f9587
Ilija Radovanovic <ilija009@gmail.com>
2022-09-21 09:41:44 +0200

    struggling with file upload

--- create_action_page_controler.php
+++ create_action_page_controler.php
@@ -7,9 +7,42 @@ require_once __DIR__ . '/models/test_input.php';
 
 $page = 'Create action page';
 
+$systemErrors = [];
+
 if(isset($_POST['create_action'])) {
-    
+    // UPLOAD: file
+
+    $allower_ext = ['png', 'jpg', 'jpeg', 'gif'];
+
+    if(!empty($_FILES['upload']['name'])) {
+        $file_name = $_FILES['upload']['name'];
+        $file_size = $_FILES['upload']['size'];
+        $file_tmp = $_FILES['upload']['tmp_name'];
+
+        $target_dir = "/public/theme/img/{$file_name}";
+
+        // get file extension
+        $file_ext = explode('.', $file_name);
+        $file_ext = strtolower(end($file_ext));
+        
+        if(in_array($file_ext, $allower_ext)) {
+            if($file_size <= 1000000) {
+                if(empty($systemErrors)) {
+                    move_uploaded_file($file_tmp, $target_dir);
+                }
+            } else {
+                $systemErrors['file_err'] = '* File is too large.';
+            }
+        } else {
+            $systemErrors['file_err'] = '* Invalid file type.';
+        }
+    } else {
+        $systemErrors['file_err'] = '* Please choose a file.';
+    }
+    // header('Location: home_page_controler.php');
 }
+
+
 
 require __DIR__ . '/views/_layout/v-header.php';
 
--- views/v-create_action.php
+++ views/v-create_action.php
@@ -1,19 +1,19 @@
 <section class="create-akcija">
     <h2>New Action</h2>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" enctype="multipart/form-data">
         <div class="form-group">
-            <label for="image">Image</label><br>
-            <input type="text" id="image" name="image">
+            <label for="upload">Image</label><br>
+            <input type="file" id="upload" name="upload">
         </div>
-        <div class="form-group">
+        <!-- <div class="form-group">
             <label for="title">Title</label><br>
             <input type="title" id="title" name="title">
-        </div>
+        </div> -->
         <!-- NOTE: change input into textarea -->
-        <div class="form-group">
+        <!-- <div class="form-group">
             <label for="description">Description</label><br>
             <input type="description" id="description" name="description">
-        </div>
+        </div> -->
         <button class="btn-login" type="submit" name="create_action">Create</button>
     </form>
 </section>
